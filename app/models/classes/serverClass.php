5ff1c23f61cf37a03af8b3273bd9954c59044af4
Zeyad Naguib <82226885+zeyadmtn@users.noreply.github.com>
2022-06-26 06:14:26 +0200

    Added MVC structure

---
+++ app/models/classes/serverClass.php
@@ -0,0 +1,12 @@
+<?php
+namespace Classes\Server;
+
+require 'app\models\interfaces\serverInterface.php';
+
+use Interfaces\Server\ServerInterface as ServerInterface;
+
+class Server1 implements ServerInterface {
+
+};
+
+?>
---
+++ app/models/interfaces/serverInterface.php
@@ -0,0 +1,7 @@
+<?php
+    namespace Interfaces\Server;
+
+    interface ServerInterface {
+
+    };
+?>
